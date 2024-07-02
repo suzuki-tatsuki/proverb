e59cd3fc44026e4486a95eb2025d68e4f03e5e9e
Kihachi2000 <kenbayashi.gaming.00@gmail.com>
2024-07-02 16:18:25 +0900

    [feat] split data by rarity

--- src/data.rs
+++ src/data.rs
@@ -16,6 +16,7 @@ impl Data {
     }
 }
 
+#[derive(PartialEq, Eq)]
 pub enum Rarity {
     Common,
     Rare,
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,24 @@ use dotenvy::dotenv;
 use std::env;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> std::result::Result<(), String> {
     dotenv().expect(".env file not loaded");
     let sheet_id = env::var("SPREAD_SHEET_ID").expect("SPREAD_SHEET_ID not found");
     let discord_token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN not found");
 
-    let data = get::get_data(&sheet_id).await;
+    let data = get::get_data(&sheet_id).await?;
+
+    let common = data.iter()
+        .filter(|x| x.rarity == data::Rarity::Common)
+        .collect::<Vec<&data::Data>>();
+    let rare = data.iter()
+        .filter(|x| x.rarity == data::Rarity::Rare)
+        .collect::<Vec<&data::Data>>();
+    let super_rare = data.iter()
+        .filter(|x| x.rarity == data::Rarity::SuperRare)
+        .collect::<Vec<&data::Data>>();
 
     send::send();
+
+    Ok(())
 }
