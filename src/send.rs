bb1b099c6b442360bdc6ac997632bed9b7d333a9
suzuki-tatsuki <suzuki.tatsuki.20@shizuoka.ac.jp>
2024-07-09 16:43:22 +0900

    [feat] add lottery func

--- src/send.rs
+++ src/send.rs
@@ -6,12 +6,33 @@ use serenity::model::gateway::Ready;
 use serenity::model::channel::Message;
 use serenity::prelude::*;
 
+use rand::Rng;
+
 struct Handler;
+
+fn lottery() {
+    // choose rarity
+    let mut rng = rand::thread_rng();
+    let n: i32 = rng.gen_range(0..10); // create number from 0-9
+
+    // 0-5(60%): common, 6-8(30%): rare, 9(10%): super-rare
+    let mut rarity: &str = "rare";
+    if n < 6 {
+        rarity = "common";
+    } else if n == 9 {
+        rarity = "super_rare";
+    }
+
+    println!("n: {}, rarity: {}", n, rarity); // for test
+}
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "proverb" {
+            lottery();
+
+            // send message
             if let Err(why) = msg.channel_id.say(&ctx.http, "test").await {
                 println!("Error sending message: {why:?}");
             }
