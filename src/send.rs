f367af4319a5f7a57b8bae3ef210edaab863a7ce
suzuki-tatsuki <suzuki.tatsuki.20@shizuoka.ac.jp>
2024-07-16 16:17:45 +0900

    [feat] start changing to only sending message

--- src/send.rs
+++ src/send.rs
@@ -1,16 +1,21 @@
 use crate::data;
 
+use serenity::http::Http;
+use serenity::model::id::ChannelId;
+
+/*  using gateway
 use serenity::async_trait;
 use serenity::model::gateway::GatewayIntents;
 use serenity::model::gateway::Ready;
 use serenity::model::channel::Message;
 use serenity::prelude::*;
+*/
 
 use rand::Rng;
 
-struct Handler;
+//struct Handler;
 
-fn lottery() {
+fn lottery() -> &'static str {
     // choose rarity
     let mut rng = rand::thread_rng();
     let n: i32 = rng.gen_range(0..10); // create number from 0-9
@@ -25,8 +30,11 @@ fn lottery() {
 
     // for debug
     println!("n: {}, rarity: {}", n, rarity);
+
+    rarity
 }
 
+/*
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
@@ -44,8 +52,22 @@ impl EventHandler for Handler {
         println!("{} is connected!", ready.user.name);
     }
 }
+*/
 
 pub async fn send(token: &str, common: Vec<&data::Data>, rare: Vec<&data::Data>, super_rare: Vec<&data::Data>) {
+    let http = Http::new(token);
+    let channel_id = ChannelId::new(123456789012345678); // ここに実際のチャンネルIDを設定
+
+    //let message_content = format!("Common data: {:?}", common::proverb);
+
+    let message_content: &str = lottery();
+
+    // sending message content to channel
+    if let Err(why) = channel_id.say(&http, message_content).await {
+        println!("Error sending message: {:?}", why);
+    }
+
+    /*  using gateway
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
@@ -58,4 +80,5 @@ pub async fn send(token: &str, common: Vec<&data::Data>, rare: Vec<&data::Data>,
     if let Err(why) = client.start().await {
         println!("client error: {why:?}");
     }
+    */
 }
