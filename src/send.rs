dc35538b4e15ebbe2ee7fe60a3a899beb5345b21
suzuki-tatsuki <suzuki.tatsuki.20@shizuoka.ac.jp>
2024-07-30 15:17:25 +0900

    [feat] able to change color of embed message

--- src/send.rs
+++ src/send.rs
@@ -22,47 +22,44 @@ pub async fn send(token: &str, id_str: &str, common: Vec<&data::Data>, rare: Vec
     let r_num: usize = lottery(10);
 
     // 0-5(60%): common, 6-8(30%): rare, 9(10%): super-rare
-    let mut rarity: &str = "rare";
+    //let mut rarity: &str = "rare";
     let content: &Data;
+    let mut embed_color: Colour = Colour(0xffffff);
 
     if r_num < 6 {
-        rarity = "common";  // for console
-
+        //rarity = "common";  // for console
         let len: usize = common.len();
         let index: usize = lottery(len);
         content = &common[index];
+        embed_color = Colour(0x816d46);
     } else if r_num == 9 {
-        rarity = "super_rare";  // for cosole
-
+        //rarity = "super_rare";  // for cosole
         let len: usize = super_rare.len();
         let index: usize = lottery(len);
         content = &super_rare[index];
+        embed_color = Colour(0xf7b946);
     } else {
         // rarity = rare;   // for console
-
         let len: usize = rare.len();
         let index: usize = lottery(len);
         content = &rare[index];
+        embed_color = Colour(0xc0c0c0);
     }
 
     let cnt_speaker = format!("{}", content.speaker);
     let cnt_proverb = format!("# 「{}」", content.proverb);
     let cnt_date = format!("{}", content.date);
     let cnt_rarity = format!("{}", content.rarity);
-    //let message_content = format!("{}\n{}「{}」({})", cnt_rarity, cnt_speaker, cnt_proverb, cnt_date);
-
-    // for console
-    //println!("r_num: {}, rarity: {}", r_num, rarity);
 
     if let Err(why) = channel_id.send_message(&http,
         CreateMessage::new()
             .content(cnt_proverb)
             .embed(
                 CreateEmbed::new()
-                    .color(Colour(0xffffff))
-                    //.title(cnt_speaker)
+                    .color(embed_color)
+                    .title(cnt_speaker)
                     .field("レア度", cnt_rarity, true)
-                    .field("発言者", cnt_speaker, true)
+                    //.field("発言者", cnt_speaker, true)
                     .field("日付", cnt_date, true)
             )
     ).await {
